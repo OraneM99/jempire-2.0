f603652eff94f0fcc4fc6fa8790e4ded73a49e64
Pierre <66121468+Zatigan@users.noreply.github.com>
2026-02-25 10:12:24 +0100

    feat: weapon class created (#55)
    Co-authored-by: BOULET Pierre <pierre.boulet@maif.fr>

---
+++ src/models/Item/Weapon.java
@@ -0,0 +1,9 @@
+package models.Item;
+
+public class Weapon extends Item {
+
+ public Weapon(String name, int damage, double efficiency, double durability) {
+  super(name, damage, efficiency, durability);
+ }
+
+}

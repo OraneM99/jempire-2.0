7a55bc7669bac48455f11c714bb9cf01a929d54e
BOULET Pierre <pierre.boulet@maif.fr>
2026-02-25 10:00:27 +0100

    feat: tool class created

--- src/models/Item/Item.java
+++ src/models/Item/Item.java
@@ -1,17 +1,19 @@
 package models.Item;
 
-public class Item {
- String name;
- int damage;
- double efficiency;
+public abstract class Item {
+ private String name;
+ private int damage;
+ private double efficiency;
+ private double durability;
 
- public Item(String name, double efficiency) {
+ public Item(String name, double efficiency, double durability) {
   this.name = name;
   this.efficiency = efficiency;
+  this.durability = 100;
  }
 
- public Item(String name, int damage, double efficiency) {
-  this(name, efficiency);
+ public Item(String name, int damage, double efficiency, double durability) {
+  this(name, efficiency, durability);
   this.damage = damage;
  }
 
@@ -38,5 +40,17 @@ public class Item {
  protected void setEfficiency(double efficiency) {
   this.efficiency = efficiency;
  }
+
+ public double getDurability() {
+  return durability;
+ }
+
+ protected void setDurability(double durability) {
+  this.durability = durability;
+ }
+
+public void useItem(double durability) {
+ System.out.println("J'utilise l'item.");
+}
 
 }
---
+++ src/models/Item/Tool.java
@@ -0,0 +1,9 @@
+package models.Item;
+
+public class Tool extends Item {
+
+ public Tool(String name, double efficiency, double durability) {
+  super(name, efficiency, durability);
+ }
+
+}
